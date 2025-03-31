af2974ef98e24be9ffbf8f4b1a42a1ab9d9eec43
Vink64 <graci.victorrocha@gmail.com>
2025-03-30 23:47:24 -0300

    :wrench: chore (package.json) : atualizando dependências

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ const pool = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(express.json())
 
 app.get("/", (req, res)=>{
 	res.status(200).json("Bem-vindo à API Codeboost 🚀")
+})
+
+app.post("/login", async(req,res)=>{
+	const user = req.body
+	
+	if(!user.email || !user.password){
+		return res.status(500).json({"message":"Dados não preenchidos corretamente."})
+	}
+
+	const result = await pool.query(`select * from users where email = '${user.email}'`)
+
+	console.log(result.rowCount)
+
+	if(result.rowCount == 0){
+		return res.status(500).json({"message":"Usuário ou senha incorretos."})
+	}
+
+	//to do: implementar tokenização jwt 
+	return res.status(200).json({"message":"Login efetuado com sucesso."})
+
 })
 
 app.listen(PORT, () => {
